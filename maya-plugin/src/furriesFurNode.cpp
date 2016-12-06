00928244be5f65ff6f7f4f5744d0e9b06b0d20b8
unknown <reimann.johan@gmail.com>
2016-12-06 11:02:25 +0100

    Added functionality for placing the nurbcurves

--- maya-plugin/src/furriesFurNode.cpp
+++ maya-plugin/src/furriesFurNode.cpp
@@ -26,6 +26,14 @@ MObject FurriesFurNode::output;
 FurriesFurNode::FurriesFurNode() {}
 FurriesFurNode::~FurriesFurNode() {}
 
+MFloatVector arrToVec(double theArray[]) {
+  MFloatVector vec;
+  vec.x = theArray[0];
+  vec.y = theArray[1];
+  vec.z = theArray[2];
+  return vec;
+}
+
 MStatus FurriesFurNode::initialize() {
   MStatus status;
   MFnTypedAttribute typedAttr;
@@ -81,10 +89,56 @@ MStatus FurriesFurNode::compute(const MPlug& plug, MDataBlock& data) {
   //cout << "NÄMEN VAD ÄR DET HÄR FÖR EN LITEN SKIT?" << endl;
   //cout << dist << endl;
 
-  for (int i = 0; i < triCount.length; i++) {
-    MFloatPoint p0 = pointList[triVert[3 * i]];
-    MFloatPoint p1 = pointList[triVert[(3 * i) + 1]];
-    MFloatPoint p2 = pointList[triVert[(3 * i) + 2]];
+  double ap0[4];
+  double ap1[4];
+  double ap2[4];
+
+  MFloatVector p0;
+  MFloatVector p1;
+  MFloatVector p2;
+  MFloatVector p3;
+  MFloatVector p4;
+  MFloatVector p2p0;
+  MFloatVector p2p1;
+  MFloatVector p0p1;
+  MFloatVector p1p0;
+  MFloatVector p2p3;
+  MFloatVector p2p4;
+
+  for (int i = 0; i < triCount.length(); i++) {
+    pointList[triVert[3 * i]].get(ap0);
+    pointList[triVert[(3 * i) + 1]].get(ap1);
+    pointList[triVert[(3 * i) + 2]].get(ap2);
+    p0 = arrToVec(ap0);
+    p1 = arrToVec(ap1);
+    p2 = arrToVec(ap2);
+
+    p2p0 = p2 - p0;
+    p2p0.normalize();
+    p2p1 = p2 - p1;
+    p2p1.normalize();
+    p0p1 = p0 - p1;
+    p0p1.normalize();
+    p1p0 = p1 - p0;
+    p1p0.normalize();
+    
+    p3 = p0 + (p2p0 * (stepSize/2)) + (p1p0 * (stepSize / 2));
+    p4 = p1 + (p2p1 * (stepSize / 2)) + (p0p1 * (stepSize / 2));
+    for (size_t i = 0; (i*stepSize) < (p0 - p2).length(); i++)
+    {
+      p2p3 = p2 - p3;
+      p2p3.normalize();
+      p2p4 = p2 - p4;
+      p2p4.normalize();
+
+      p3 = p2p3 * (stepSize * i);
+      p4 = p2p4 * (stepSize * i);
+      for (int k = 0; (k*stepSize) < (p4-p3).length(); k++) {
+        // Add nurbcurve at p3 + ((p4-p3).normalize() * k * stepSize)
+      }
+    }
+
+    (stepSize / 2) + stepSize * i;
   }
 
   return status;
